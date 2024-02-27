d0e955c41bbdb508928724ea7a6927071e4508e7
wassimObaziz <wassimdjaafar5@gmail.com>
2024-02-27 17:58:54 +0100

    Add Flowbite-React and React-Icons dependencies, update Home component, and add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignIn from "./pages/home/SignIn";
 import SignUp from "./pages/home/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="sign-in" element={<SignIn />} />
