6b3a73431ce2a8b95773c8bb798c1cf03e848867
wassimObaziz <wassimdjaafar5@gmail.com>
2024-02-27 15:48:17 +0100

    adding some pages and installing react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SignIn from "./pages/home/SignIn";
+import SignUp from "./pages/home/SignUp";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold text-red-700">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
